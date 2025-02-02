53cd7ac1a43f7c237eb23c691826b91e63149e71
victoriaag6 <victoriaacostag6@gmail.com>
2025-02-02 15:09:20 -0400

    lieufnwpinef

--- program/obj/Debug/net9.0/program.AssemblyInfo.cs
+++ program/obj/Debug/net9.0/program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7cc60f1c1ff563ff874f4f84f67c8c9c0fa9c54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d710cd7a12ff10f1e99799f8b09e654d103f4a82")]
 [assembly: System.Reflection.AssemblyProductAttribute("program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
